cf9aa6b56b5b3ead8cb09ae03f34974daf5e6c26
samit-ahlawat <85273069+samit-ahlawat@users.noreply.github.com>
2026-03-10 17:59:21 -0400

    Add files via upload

---
+++ defenses.py
@@ -0,0 +1,240 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from enum import Enum
+from typing import Callable, Any
+
+class DefenseType(Enum):
+    DISTILLATION = 0
+    TRADES = 1,
+    MART = 2
+
+class Defenses:
+    def __init__(self,
+                 beta: float = 6.0, # used in TRADES defense
+                 eps: float = 8/255, 
+                 alpha: float = 2/255, 
+                 iters: int = 10,
+                 device: torch.device | None = None,
+                 teacher_model: torch.nn.Module | None = None, # used in defensive distillation
+                 temperature: float = 20,
+                ) -> None:
+        self.beta = beta
+        self.eps = eps
+        self.alphs = alpha
+        self.iters = iters
+        self.device = device
+        if self.device is None:
+            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.teacher_model = teacher_model
+        self.temperature = temperature
+        
+    def pgd_attack(self, 
+                   model: torch.nn.Module, 
+                   x: torch.tensor, 
+                   y: torch.tensor) -> torch.tensor:
+        x_adv = x.detach() + torch.zeros_like(x).uniform_(-self.eps, self.eps)
+        x_adv = torch.clamp(x_adv, 0, 1)
+    
+        for _ in range(self.iters):
+            x_adv.requires_grad_()
+    
+            logits = model(x_adv)
+            loss = F.cross_entropy(logits, y)
+    
+            grad = torch.autograd.grad(loss, x_adv)[0]
+    
+            x_adv = x_adv.detach() + self.alpha * torch.sign(grad)
+            x_adv = torch.min(torch.max(x_adv, x - self.eps), x + self.eps)
+            x_adv = torch.clamp(x_adv, 0, 1)
+    
+        return x_adv
+
+    def adversarial_training(self, 
+                             model: torch.nn.Module, 
+                             x: torch.tensor, 
+                             y: torch.tensor, 
+                             optimizer: torch.optim.Optimizer) -> float:
+        model.train()
+    
+        x_adv = self.pgd_attack(model, x, y)
+    
+        logits_adv = model(x_adv)
+    
+        loss = F.cross_entropy(logits_adv, y)
+    
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    
+        return loss.item()
+
+    def defensive_distillation_loss(self,
+                    model: torch.nn.Module, 
+                    x: torch.tensor, 
+                    y: torch.tensor, # not used
+                    optimizer: torch.optim.Optimizer) -> float:
+
+        model.train()
+
+        with torch.no_grad():
+            teacher_logits = self.teacher_model(x)
+            soft_targets = F.softmax(teacher_logits/self.temperature, dim=1)
+
+        student_logits = model(x)
+
+        loss = F.kl_div(
+            F.log_softmax(student_logits/self.temperature, dim=1),
+            soft_targets,
+            reduction="batchmean"
+        ) * (self.temperature * self.temperature)
+    
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    
+        return loss.item()
+
+    def trades_loss(self,
+                    model: torch.nn.Module, 
+                    x: torch.tensor, 
+                    y: torch.tensor, 
+                    optimizer: torch.optim.Optimizer) -> float:
+
+        # loss = CE(f(x), y) + beta * KullbackLeibler(f(x_adv) || f(x))
+        model.train()
+    
+        x_adv = x.detach() + 0.001 * torch.randn_like(x)
+    
+        for _ in range(iters):
+            x_adv.requires_grad_()
+    
+            loss_kl = F.kl_div(
+                F.log_softmax(model(x_adv), dim=1),
+                F.softmax(model(x), dim=1),
+                reduction='batchmean'
+            )
+    
+            grad = torch.autograd.grad(loss_kl, x_adv)[0]
+    
+            x_adv = x_adv.detach() + self.alpha * torch.sign(grad)
+            x_adv = torch.min(torch.max(x_adv, x - self.eps), x + self.eps)
+            x_adv = torch.clamp(x_adv, 0, 1)
+    
+        logits = model(x)
+        logits_adv = model(x_adv)
+    
+        loss_natural = F.cross_entropy(logits, y)
+    
+        loss_robust = F.kl_div(
+            F.log_softmax(logits_adv, dim=1),
+            F.softmax(logits, dim=1),
+            reduction='batchmean'
+        )
+    
+        loss = loss_natural + self.beta * loss_robust
+    
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    
+        return loss.item()
+
+    def mart_loss(self,
+                  model: torch.nn.Module, 
+                  x: torch.tensor, 
+                  y: torch.tensor, 
+                  optimizer: torch.optim.Optimizer) -> float:
+
+        # x_adv comes from PDF attack
+        # loss focusses on misclassified examples and on margin-aware loss for correctly classified examples
+        # Misclassification Aware adveRsarial Training (MART)
+        model.train()
+    
+        x_adv = self.pgd_attack(model, x, y)
+    
+        logits = model(x)
+        logits_adv = model(x_adv)
+    
+        prob = F.softmax(logits, dim=1)
+        prob_adv = F.softmax(logits_adv, dim=1)
+    
+        ce_loss = F.cross_entropy(logits_adv, y)
+    
+        tmp1 = torch.argsort(prob_adv, dim=1)[:, -2:]
+        new_y = torch.where(tmp1[:, -1] == y, tmp1[:, -2], tmp1[:, -1])
+    
+        loss_adv = ce_loss + F.nll_loss(
+            torch.log(1.0001 - prob_adv + 1e-12),
+            new_y
+        )
+    
+        true_prob = prob.gather(1, y.unsqueeze(1)).squeeze()
+    
+        kl = F.kl_div(
+            torch.log(prob_adv + 1e-12),
+            prob,
+            reduction='none'
+        ).sum(dim=1)
+    
+        loss_robust = (kl * (1 - true_prob)).mean()
+    
+        loss = loss_adv + self.beta * loss_robust
+    
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    
+        return loss.item()
+
+    def get_defense(self, 
+                    defense_type: DefenseType,
+                    data_loader: torch.utils.data.DataLoader,
+                    epochs: int,
+                   ) -> Callable[..., Any]:
+        if defense_type == DefenseType.DISTILLATION:
+            self.train_teacher_model(data_loader, epochs)
+            return self.defensive_distillation_loss
+        elif defense_type == DefenseType.TRADES:
+            return self.trades_loss
+        elif defense_type == DefenseType.MART:
+            return self.mart_loss
+        raise ValueError(f"Unsupported defense type {defense_type}")
+
+    def train_teacher_model(self, 
+                            data_loader: torch.utils.data.DataLoader,
+                            epochs: int,
+                           ):
+
+    self.teacher_model.train()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
+
+    for _ in range(epochs):
+        for x,y in data_loader:
+            logits = self.teacher_model(x)
+    
+            loss = F.cross_entropy(logits/self.temperature, y)
+    
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+    self.teacher_model.eval()
+
+    def apply_defense(self,
+                      model: torch.nn.Module, 
+                      optimizer: torch.optim.Optimizer,
+                      data_loader: torch.utils.data.DataLoader,
+                      epochs: int,
+                      defense_type: DefenseType
+                     ) -> None:
+        defense = self.get_defense(defense_type, data_loader, epochs)
+        
+        for epoch in range(epochs):
+            epoch_loss = 0
+            for x, y in data_loader:
+                x, y = x.to(device), y.to(device)
+                loss = defense(model, x, y, optimizer)
+                epoch_loss += loss
+            print(f"Epoch {epoch} | Loss {epoch_loss:.4f}")
---
+++ resnet50MNist.py
@@ -0,0 +1,170 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms, models
+from torch.utils.data import DataLoader
+from dataclasses import dataclass
+
+@dataclass
+class RunArgs:
+    batch_size: int = 32
+    test_batch_size: int = 32
+    epochs: int = 5
+    lr: float = 0.001
+    gamma: float = 0.9
+    no_accel: bool = False
+    dry_run: bool = False
+    seed: int = 32
+    log_interval: int = 1000
+    save_model_name: str | None = None
+
+class PretrainedResnet50:
+    def __init__(self, num_classes, args):
+        self.args = args
+        self.num_classes = num_classes
+        use_accel = not args.no_accel and torch.accelerator.is_available()
+
+        torch.manual_seed(args.seed)
+        if use_accel:
+            self.device = torch.accelerator.current_accelerator()
+        else:
+            self.device = torch.device("cpu")
+        self.setup(num_classes)
+
+    def load_pretrained_model(self, num_classes):
+        # -----------------------
+        # Load pretrained model
+        # -----------------------
+        model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
+        
+        # Replace classification head fully connected layer
+        in_features = model.fc.in_features
+        model.fc = nn.Linear(in_features, num_classes)
+        
+        model = model.to(self.device)
+        return model
+
+    def setup(self, num_classes):
+        self.model = self.load_pretrained_model(num_classes)
+        self.criterion = nn.CrossEntropyLoss()
+
+        self.optimizer = optim.AdamW(
+            self.model.parameters(),
+            lr=self.args.lr,
+        )
+        
+        self.scheduler = optim.lr_scheduler.StepLR(
+            self.optimizer,
+            step_size=1,
+            gamma=self.args.gamma
+        )
+
+
+        self.transform = transforms.Compose([
+            transforms.Resize(224),            # ResNet input size
+            transforms.Grayscale(num_output_channels=3),  # 1 -> 3 channels
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225]
+            )
+        ])
+        
+        # -------------------
+        # Dataset
+        # -------------------
+        train_dataset = datasets.MNIST(
+            root="./data",
+            train=True,
+            download=True,
+            transform=self.transform
+        )
+        
+        test_dataset = datasets.MNIST(
+            root="./data",
+            train=False,
+            download=True,
+            transform=self.transform
+        )
+        
+        self.train_loader = DataLoader(train_dataset, batch_size=self.args.batch_size, shuffle=True)
+        self.test_loader = DataLoader(test_dataset, batch_size=self.args.test_batch_size, shuffle=False)
+
+    def train_one_epoch(self):
+        self.model.train()
+        running_loss = 0.0
+        correct = 0
+        total = 0
+    
+        for images, labels in self.train_loader:
+            images = images.to(self.device)
+            labels = labels.to(self.device)
+    
+            self.optimizer.zero_grad()
+    
+            outputs = self.model(images)
+            loss = self.criterion(outputs, labels)
+    
+            loss.backward()
+            self.optimizer.step()
+    
+            running_loss += loss.item()
+    
+            _, preds = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (preds == labels).sum().item()
+    
+        acc = 100 * correct / total
+        return running_loss / len(self.train_loader), acc
+    
+    
+    # -----------------------
+    # Validation loop
+    # -----------------------
+    def validate(self):
+        self.model.eval()
+        correct = 0
+        total = 0
+    
+        with torch.no_grad():
+            for images, labels in self.test_loader:
+                images = images.to(self.device)
+                labels = labels.to(self.device)
+    
+                outputs = self.model(images)
+                _, preds = torch.max(outputs, 1)
+    
+                total += labels.size(0)
+                correct += (preds == labels).sum().item()
+    
+        return 100 * correct / total
+
+
+    def finetune(self):
+        # -----------------------
+        # Main training
+        # -----------------------
+        for epoch in range(self.args.epochs):
+        
+            train_loss, train_acc = self.train_one_epoch()
+            val_acc = self.validate()
+        
+            self.scheduler.step()
+        
+            print(
+                f"Epoch [{epoch+1}/{self.args.epochs}] "
+                f"Loss: {train_loss:.4f} "
+                f"Train Acc: {train_acc:.2f}% "
+                f"Val Acc: {val_acc:.2f}%"
+            )
+        
+        # -----------------------
+        # Save model
+        # -----------------------
+        if self.args.save_model_name:
+            torch.save(self.model.state_dict(), f"{self.args.save_model_name}.pth")
+
+if __name__ == "__main__":
+    args = RunArgs()
+    pt = PretrainedResnet50(num_classes=10, args=args)
+    pt.finetune()
